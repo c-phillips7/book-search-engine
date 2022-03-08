57c55b7436e2bd0cbadd0a4a4408f9e344f340d9
Chris Phillips <cp.phillips15@gmail.com>
2022-03-08 18:22:14 -0500

    added logic for query  of user then populate book, but hard to test empty database

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,27 @@
-const { signToken } = require('../utils/auth')
-const { User } = require('../models');
+const {
+    signToken
+} = require('../utils/auth')
+const {
+    User
+} = require('../models');
+const {
+    AuthenticationError
+} = require('apollo-server-express');
 
 const resolvers = {
-    // Query: {
-
-    // }
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({
+                    _id: context.user._id
+                }).populate(
+                    'savedbooks'
+                );
+                return userData;
+            }
+            throw new AuthenticationError('not logged in');
+        },
+    },
 
 };
 
